bb88c3486adb2b723bfb77b42e48c77f74567a75
Gai Gatluak Deng <gdeng8@my.bcit.ca>
2026-07-02 21:37:13 -0700

    feat: integrated the database connection test to the unit test suite

--- app/database/check_connection.py
+++ app/database/check_connection.py
@@ -1,8 +1,23 @@
-from session import my_engine
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
 
-try:
-    conn = my_engine.connect()
-    print("✅ DB CONNECTED")
-    conn.close()
-except Exception as e:
-    print("❌ DB FAILED:", e)
+load_dotenv()
+
+db_url = os.getenv("DATABASE_URL")
+
+def connect_db(db_url):
+    try:
+        engine = create_engine(db_url)
+        with engine.connect() as conn:
+            results = conn.execute(text("SELECT 1"))
+            print("Connection to the database successful✅✅")
+            for result in results:
+                print(result)
+            return True
+    except Exception as ex:
+        print(f"❌❌ Failed to connected to db due to error:\n{str(ex).splitlines()[0]}")
+        return False
+    
+if __name__ == '__main__':
+    print(connect_db())
